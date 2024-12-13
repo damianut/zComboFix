9af5b1a157f2cd9a12ff1472979c329e6ca20be1
damianut <damianas1999@gmail.com>
2024-12-13 13:43:33 +0100

    Fight Master style for Gothic 2 only

--- Ninja/zComboFix/Fight/FAI/FAI_Human_Master.d
+++ Ninja/zComboFix/Fight/FAI/FAI_Human_Master.d
@@ -70,8 +70,10 @@ instance FA_MY_W_FOCUS_4(C_FightAI)
 {
 	// move[0] = MOVE_FRONTATTACK;
 	// move[1] = MOVE_ATTACK;
-	// move[2] = MOVE_WAIT; // --- Wait sorgt für besseres Parade-Fenster ---
-    move[0] = MOVE_MASTERATTACK;
+    // move[2] = MOVE_WAIT; // --- Wait sorgt für besseres Parade-Fenster ---
+    
+    move[0] = MOVE_MASTERATTACK; // Gothic 1
+    move[1] = MOVE_WAIT; // --- Wait sorgt für besseres Parade-Fenster ---
 };
 
 // ------- Ich habe Gegner NICHT im Fokus -------
